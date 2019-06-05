5faf10ca0b4262c78282109be981beae86a0b1a9
Peter <phefland@gmail.com>
2019-06-05 18:58:50 -0300

    navbar with toggle

---
+++ src/constants/social-icons.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaFacebook, FaTwitterSquare, FaYoutubeSquare} from 'react-icons/fa'
+
+export default [
+    {
+        icon:<FaFacebook/>,
+        url:'https://google.com'
+    },
+    {
+        icon: <FaTwitterSquare />,
+        url: 'https://twitter.com'
+    },
+    {
+        icon: <FaYoutubeSquare />,
+        url: 'https://youtube.com'
+    },
+]
